aa818dcaebf544fa0a9db64b64ecc82621719fd9
s-erittu <AnnaPintus16>
2025-07-29 17:20:10 +0200

    Primo commit: progetto CCS con polling LED

---
+++ main.asm
@@ -0,0 +1,66 @@
+;-------------------------------------------------------------------------------
+; MSP430 Assembler Code Template for use with TI Code Composer Studio
+;
+;
+;-------------------------------------------------------------------------------
+            .cdecls C,LIST,"msp430.h"       ; Include device header file
+            
+;-------------------------------------------------------------------------------
+            .def    RESET                   ; Export program entry-point to
+                                            ; make it known to linker.
+;-------------------------------------------------------------------------------
+            .text                           ; Assemble into program memory.
+            .retain                         ; Override ELF conditional linking
+                                            ; and retain current section.
+            .retainrefs                     ; And retain any sections that have
+                                            ; references to current section.
+
+;-------------------------------------------------------------------------------
+RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
+StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
+
+
+;-------------------------------------------------------------------------------
+; Main loop here
+;-------------------------------------------------------------------------------
+
+init:
+			bis.b	#BIT0, &P1DIR			; setup P1.0 as output. P1.0 = LED1
+			bic.b	#BIT0, &P1OUT			; set initial value of LED1 = OFF
+
+			bic.b	#BIT1, &P4DIR			; setup P4.1 as input. P4.1 = interruttore (S1)
+			bis.b 	#BIT1, &P4REN			; enable pull up/down resistor on P4.1
+			bis.b	#BIT1, &P4OUT			; conf resistor as pull up
+
+			bic.b	#LOCKLPM5, &PM5CTL0		; enable digital I/O
+
+main:
+
+poll_S1:
+			bit.b	#BIT1, &P4IN			; test P4.1
+			jnz		poll_S1					; stay in polling loop
+
+toggle_LD1:
+			xor.b	#BIT0, &P1OUT			; toggle LED1
+
+;--------------------------------
+			mov.w	#0FFFFh, R4
+delay:
+			dec.w	R4
+			jnz		delay
+;--------------------------------
+
+			jmp		main
+			nop
+;-------------------------------------------------------------------------------
+; Stack Pointer definition
+;-------------------------------------------------------------------------------
+            .global __STACK_END
+            .sect   .stack
+            
+;-------------------------------------------------------------------------------
+; Interrupt Vectors
+;-------------------------------------------------------------------------------
+            .sect   ".reset"                ; MSP430 RESET Vector
+            .short  RESET
+            
